8c01b3806d00a4e82bec170e0f28de47da56d0f5
samvekemans <sam.vekemans@hva.nl>
2022-05-12 11:28:07 +0200

    changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   var mascots = [
     { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
     { name: 'Tux', organization: "Linux", birth_year: 1996},
@@ -21,16 +21,13 @@ app.get('/', function(req, res) {
   });
 });
 
-// about page
-app.get('/about', function(req, res) {
-  var name = 'hello'
 
-  res.render('pages/about', {
-    names: name
-  });
-});
+
+app.use((req, res, next) => {
+  res.send('Pagina niet gevonden', 404)
+})
+
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
-console.log('Server is listening on port 8080');
