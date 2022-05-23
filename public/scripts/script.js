851fdf56b5991715d455a94feef698785fcbfb86
samvekemans <sam.vekemans@hva.nl>
2022-05-23 10:41:05 +0200

    back-end

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,11 +1,16 @@
 const pictureInput = document.getElementById('pictureUser');
 const img = document.querySelector('.img');
 
-pictureInput.addEventListener('change', (event) => {
-  if (event.target.files.length !== 0) {
-    const imgFile = event.target.files[0];
-    const imgSrc = URL.createObjectURL(imgFile);
+function profilePicture() {
+  if (pictureInput) {
+    pictureInput.addEventListener('change', (event) => {
+      if (event.target.files.length !== 0) {
+        const imgFile = event.target.files[0];
+        const imgSrc = URL.createObjectURL(imgFile);
 
-    img.src = imgSrc;
+        img.src = imgSrc;
+      }
+    });
   }
-});
+}
+profilePicture();
--- server.js
+++ server.js
@@ -1,27 +1,44 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const { ObjectId } = require('mongodb');
+let  db;
 
 const app = express();
-const data = [];
 const port = process.env.PORT || 3000;
+let query = {};
 
 app.use(express.static(`${__dirname}/public`));
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('pages/index', {
-    data,
-  });
+  
+app.get('/', async (req, res) => {
+  
+  res.render('pages/index');
 });
 
 app.get('/ouderen', (req, res) => {
-  res.render('pages/old-people', {
-    data,
+  const radioTitle = 'Waar zoekt u naar?';
+  const slider = {
+    title: 'Ervaring Zorgmedewerker',
+    min: 1,
+    max: 100,
+  };
+  res.render('pages/register', {
+    radioTitle,
+    slider,
   });
 });
 
 app.get('/zorgmedewerker', (req, res) => {
-  res.render('pages/care-giver', {
-    data,
+  const radioTitle = 'Wat wil je doen?';
+  const slider = {
+    title: 'Leeftijd van de oudere',
+    min: 50,
+    max: 100,
+  };
+  res.render('pages/register', {
+    radioTitle,
+    slider,
   });
 });
 
@@ -29,18 +46,59 @@ app.get('/error', (req, res) => {
   res.render('pages/error');
 });
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.get('/users', async(req, res) => {
+  const users = await db.collection("profiles").find(query).toArray();
 
-app.post('/', (req, res) => {
-  data.push({
+  res.render('pages/users', {
+    users,
+  })
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', async(req, res) => {
+  let user = {
     name: req.body.name,
-  });
-  res.redirect('/');
+    age: req.body.age,
+    aboutUser: req.body.description,
+    help: req.body.help
+  }
+
+  try{
+    await db.collection("profiles").insertOne(user);
+  }
+  catch(error){
+    console.log(error);
+  }
+  
+  query = {
+    help: req.body.help
+  },
+
+  res.redirect('/users');
 });
 
 app.use((req, res, next) => {
+  res.status(404);
   res.redirect('/error');
 });
 
-app.listen(port);
+async function conectDB(){
+  const uri = process.env.DB_URI;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  try {
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+  }
+  catch (error){
+    throw error
+  }
+};
+
+app.listen(port, () => {
+  conectDB()
+});
