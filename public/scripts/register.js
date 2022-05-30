a0dfa7f85372fe4f36a156efc30fcaf35cf313a8
samvekemans <sam.vekemans@hva.nl>
2022-05-30 20:00:46 +0200

    everything

---
+++ public/scripts/register.js
@@ -0,0 +1,43 @@
+const pictureInput = document.getElementById('pictureUser');
+const img = document.querySelector('.img');
+const oldButton = document.querySelector('#old-person');
+const careButton = document.querySelector('#caregiver');
+
+function profilePicture() {
+  if (pictureInput) {
+    pictureInput.addEventListener('change', (event) => {
+      if (event.target.files.length !== 0) {
+        const imgFile = event.target.files[0];
+        const imgSrc = URL.createObjectURL(imgFile);
+
+        img.src = imgSrc;
+      }
+    });
+  }
+}
+profilePicture();
+
+function styleButton() {
+  if(window.location.pathname === '/ouderen'){
+    oldButton.style.backgroundColor = '#519188';
+    oldButton.style.boxShadow = '0px 0px 5px #519188';
+    oldButton.style.fontWeight = 'bold';
+    oldButton.classList.add('selected');
+
+    careButton.style.backgroundColor = 'white';
+    careButton.style.boxShadow = 'none';
+    careButton.style.fontWeight = 'normal';
+  }
+  if(window.location.pathname === '/zorgmedewerker'){
+    careButton.style.backgroundColor = '#519188';
+    careButton.style.boxShadow = '0px 0px 5px #519188';
+    careButton.style.fontWeight = 'bold';
+    careButton.classList.add('selected');
+
+    oldButton.style.backgroundColor = 'white';
+    oldButton.style.boxShadow = 'none';
+    oldButton.style.fontWeight = 'normal';
+  }
+}
+
+styleButton()
--- server.js
+++ server.js
@@ -2,43 +2,71 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const { ObjectId } = require('mongodb');
-let  db;
 
-const app = express();
+const path = require('path')
+const multer  = require('multer')
+const storage = multer.diskStorage({
+  destination: `${__dirname}/public/uploads`,
+  filename: function (req, file, cb) {
+      cb(null, file.fieldname + Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({
+      storage: storage
+  }).single('pictureUser');
+
+let  db;
 const port = process.env.PORT || 3000;
 let query = {};
+let oldTradesTitle = '';
+let careTradesTitle = '';
+
+const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 app.set('view engine', 'ejs');
   
 app.get('/', async (req, res) => {
-  
   res.render('pages/index');
 });
 
 app.get('/ouderen', (req, res) => {
-  const radioTitle = 'Waar zoekt u naar?';
-  const slider = {
-    title: 'Ervaring Zorgmedewerker',
-    min: 1,
-    max: 100,
+  const iconTitles = {
+    first: 'Welke eigenschappen verwacht je van de zorgmedewerker?',
+    second: 'Welke eigenschappen passen het beste bij jou?'
   };
+  const valueType = "ouderen";
+  const radioTitle = 'Waar zoekt u naar?';
+  const trades = { 
+    first: 'oldPersonTrades[]',
+    second: 'careGiverTrades[]'
+  };
+
   res.render('pages/register', {
     radioTitle,
-    slider,
+    valueType,
+    trades,
+    iconTitles,
   });
 });
 
 app.get('/zorgmedewerker', (req, res) => {
-  const radioTitle = 'Wat wil je doen?';
-  const slider = {
-    title: 'Leeftijd van de oudere',
-    min: 50,
-    max: 100,
+  const iconTitles = {
+    first: 'Welke eigenschappen passen het beste bij jou?',
+    second: 'Welke eigenschappen verwacht je van de oudere persoon?'
   };
+  const valueType = "zorgmedewerker";
+  const radioTitle = 'Wat wil je doen?';
+  const trades = { 
+    first: 'careGiverTrades[]',
+    second: 'oldPersonTrades[]'
+  };
+
   res.render('pages/register', {
     radioTitle,
-    slider,
+    valueType,
+    trades,
+    iconTitles,
   });
 });
 
@@ -51,29 +79,47 @@ app.get('/users', async(req, res) => {
 
   res.render('pages/users', {
     users,
+    oldTradesTitle,
+    careTradesTitle,
   })
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/', async(req, res) => {
+app.post('/ouderen', upload, async(req, res) => {
   let user = {
+    accountType: req.body.account,
     name: req.body.name,
     age: req.body.age,
     aboutUser: req.body.description,
-    help: req.body.help
+    help: req.body.help,
+    image: 'uploads/' + req.file.filename,
+    oldTrades: req.body.oldPersonTrades,
+    careTrades: req.body.careGiverTrades
   }
 
   try{
     await db.collection("profiles").insertOne(user);
   }
   catch(error){
-    console.log(error);
+    throw error
   }
-  
+
+  if(req.body.account === 'ouderen'){
+    careTradesTitle = 'Mijn eigenschappen:'
+    oldTradesTitle = 'De zorgmedewerker wil dit graag van de ouderen:'
+  }
+  if(req.body.account === 'zorgmedewerker'){
+    oldTradesTitle = 'Mijn eigenschappen:'
+    careTradesTitle = 'Wat de ouderen verwacht van jou:'
+  }
+
   query = {
-    help: req.body.help
+    accountType: req.body.account === 'ouderen' ? 'zorgmedewerker' : 'ouderen',
+    oldTrades:{ $in: req.body.oldPersonTrades},
+    careTrades:{ $in: req.body.careGiverTrades},
+    help: {$in: req.body.help},
   },
 
   res.redirect('/users');
